f2d3f849f56e003f7b05590b1c11495fbb374114
ByteyWolf <235580007+ByteyWolf@users.noreply.github.com>
2025-12-20 22:45:35 +0200

    file reading! well only the first cluster

--- boot/stage1.asm
+++ boot/stage1.asm
@@ -17,59 +17,51 @@ load_fat12:
     mov bl, [0x7C10]  ; num FATs
     xor bh, bh
     mov cx, [0x7C0E]  ; reserved sectors
+    mov dx, [0x7C11]  ; max root entries
+    mov [numsectorsperfat], ax
+    mov [numfats], bl
+    mov [numreserved], cx
+    mov [nummaxrootentries], dx
 
-    ; preserve
-    push cx
-    push bx
-    push ax
+    mov ax, dx
+    mov bx, 32              ; bytes per entry
+    mul bx                  ; AX = total bytes for root
+    mov bx, 512             ; bytes per sector
+    cwd                     ; DX:AX for division
+    div bx                  ; AX = number of root sectors
+    mov [rootsectors], ax
 
     mov si, 0x7CE9
     call 0x7CA0
 
-    push bp
-    mov bp, sp
-
     ; print sectors per fat
-    mov ax, [bp+2]
+    mov ax, [numsectorsperfat]
     call print_hex16
     mov ah, 0x0E
     mov al, 0x20
     int 0x10
 
-    mov ax, [bp+4]
-    call print_hex16
+    mov al, [numfats]
+    call print_hex8
     mov ah, 0x0E
     mov al, 0x20
     int 0x10
 
-    mov ax, [bp+6]
+    mov ax, [numreserved]
     call print_hex16
 
     call newline
 
-    pop bp
-    pop ax
-    pop bx
-    pop cx
+    mov ax, [numsectorsperfat]
+    mov bx, [numfats]
+    mov cx, [numreserved]
 
 
     mul bx
     add ax, cx
 
     ; convert into chs!!
-    xor dx, dx
-    mov bx, 18          ; sectors per track
-    div bx              ; AX = temp, DX = remainder
-
-    inc dx              ; sector = remainder + 1
-    mov cl, dl          ; CL = sector
-
-    xor dx, dx
-    mov bx, 2           ; heads
-    div bx              ; AX = cylinder, DX = head
-
-    mov ch, al          ; cylinder
-    mov dh, dl          ; head
+    call lba2chs
 
 
 
@@ -158,8 +150,49 @@ load_fat12:
     mov si, 0x7D24
     call 0x7CA0
     pop si
+
     mov ax, [si+26]
+    mov cx, ax
     call print_hex16
+    call newline
+    call newline
+
+    cmp cx, 0
+    je .notfound
+
+    mov ax, [si+28]
+    push ax
+
+    mov ax, [numfats]
+    mul word [numsectorsperfat]
+    add ax, [numreserved]
+    add ax, [rootsectors]
+    mov bx, ax
+
+    mov ax, cx
+    sub ax, 2
+    add ax, bx
+
+    call lba2chs
+
+
+    xor ax, ax
+    mov es, ax
+    mov dl, [driveid]
+    mov bx, 0x9000
+    mov ah, 2
+    mov al, 1
+
+
+    int 0x13
+
+    jc disk_error
+
+    mov si, 0x9000
+    pop dx
+    call printtimes
+
+    call newline
 
     hlt
     jmp $
@@ -171,6 +204,29 @@ driveid db 0
 fileid db 0
 numheads dw 0
 numsectorspertrack dw 0
+
+numsectorsperfat dw 0
+numfats db 0
+numreserved dw 0
+nummaxrootentries dw 0
+
+rootsectors dw 0
+
+lba2chs:
+    xor dx, dx
+    mov bx, 18          ; sectors per track
+    div bx              ; AX = temp, DX = remainder
+
+    inc dx              ; sector = remainder + 1
+    mov cl, dl          ; CL = sector
+
+    xor dx, dx
+    mov bx, 2           ; heads
+    div bx              ; AX = cylinder, DX = head
+
+    mov ch, al          ; cylinder
+    mov dh, dl          ; head
+    ret
 
 newline:
     mov ah, 0x0E
